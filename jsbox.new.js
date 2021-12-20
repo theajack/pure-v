c924a27b9221ec5a2d75a30962aa036ef4ab8b0f
Tack Chen <1506282385@qq.com>
2021-12-20 15:42:11 +0800

    Create jsbox.new.js

---
+++ jsbox.new.js
@@ -0,0 +1,15 @@
+window.jsboxCode = {
+	lib: "https://cdn.jsdelivr.net/npm/pure-v/purev.min.js",
+    code: /* html*/`console.log(purev('2020-01-01','date'));
+console.log(purev({
+	name:'theajack',
+	birthday: '1994-01-01',
+	email:'me@theajack.com',
+	intro:'Programmer'
+}, {
+	name:'notnull',
+	birthday:'date',
+	email:'email',
+	intro:'notnull'
+}));`
+};
