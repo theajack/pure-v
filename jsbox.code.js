e341107c1a0edb51d4e97538f8d69df21a2eec84
Tack Chen <1506282385@qq.com>
2021-12-20 15:41:39 +0800

    Update jsbox.code.js

--- jsbox.code.js
+++ jsbox.code.js
@@ -1,15 +1,26 @@
 window.jsboxCode = {
-	lib: "https://cdn.jsdelivr.net/npm/pure-v/purev.min.js",
-    code: /* html*/`console.log(purev('2020-01-01','date'));
-console.log(purev({
-	name:'theajack',
-	birthday: '1994-01-01',
-	email:'me@theajack.com',
-	intro:'Programmer'
-}, {
-	name:'notnull',
-	birthday:'date',
-	email:'email',
-	intro:'notnull'
-}));`
+    code: /* html*/`<script src="https://cdn.jsdelivr.net/npm/pure-v/purev.min.js"></script>
+<div pv-form='form'>
+    name:<input type="text" pv-rule='notnull'><br><br>
+    birthday:<input type="text" pv-rule='date'><br><br>
+    <button onclick='valid()'>validate Form</button>
+</div>
+<script>
+    console.log(purev('2020-01-01','date'));
+    console.log(purev({
+        name:'theajack',
+        birthday: '1994-01-01',
+        email:'me@theajack.com',
+        intro:'Programmer'
+    }, {
+        name:'notnull',
+        birthday:'date',
+        email:'email',
+        intro:'notnull'
+    }));
+    function valid (){
+        purev('form');
+    }
+</script>`,
+    lang: 'html'
 };
